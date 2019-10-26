3bf23f25e14786aea82da46b85eb070dd343c7d7
wildanm <wildan.m@gmail.com>
2019-10-27 06:27:44 +0700

    debug

--- R/itemClassic.R
+++ R/itemClassic.R
@@ -72,7 +72,6 @@ function(thr, yRange = NULL, axis.items = "Items",axis.logits = "Logits",show.ax
 	# item.labels - use generic list or user defined item names 
 	if (item.names.labels==FALSE){
 	    item.labels <- matrix(rep(formatC(1:nI, digits = 1, format = "d", flag = "0"), nL), ncol = nL)
-		print item.labels
 		print formatC(1:nI, digits = 1, format = "d", flag = "0")
 	}else if(item.names.labels==TRUE){
 		print(throrig)
